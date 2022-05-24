b884883633123efecf5aecce5c072ae50d6e23ae
Natalie Bulger <bulgern20@gmail.com>
2022-05-24 14:55:58 -0700

    File creation, initial html, hbs, and models

---
+++ models/Blog.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
+
+class Blog extends Model {}
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    keyPhrase: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
+
+module.exports = Blog;
