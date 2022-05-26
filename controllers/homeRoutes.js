80bb8bb41d8a7ebcefb9806bbb6a71f5735fec3c
Natalie Bulger <bulgern20@gmail.com>
2022-05-25 18:33:46 -0700

    Route updates

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -26,4 +26,38 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/login", async (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  //   if (req.session.logged_in) {
+  //     res.redirect("/profile");
+  //     return;
+  //   }
+
+  res.render("login");
+});
+
+router.get("/dashboard", async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const blogData = await Blog.findAll({
+      include: [
+        {
+          model: User,
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render("dashboard", {
+      blogs,
+      //   logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
